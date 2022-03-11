c95f849e2dd8506bec170f4ac327fc7337b707cc
sourabh <51184750+sourabhpatole@users.noreply.github.com>
2022-03-11 10:02:03 +0530

    complete env user n product model

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello is there");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connection successful"))
+  .catch((error) => console.log(error));
+app.use(express.json());
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Backend Server is Running on the port`);
+});
