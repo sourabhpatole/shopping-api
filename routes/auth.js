254ac8051f1875be4c0c41aefb8fd5a96ed037a0
sourabh <51184750+sourabhpatole@users.noreply.github.com>
2022-03-18 16:20:58 +0530

    final

--- routes/auth.js
+++ routes/auth.js
@@ -25,7 +25,9 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    !user && res.status(401).json("Wrong credentials!");
+    if (!user) {
+      return res.status(401).json("Wrong credentials!");
+    }
     const hashPassword = CryptoJS.AES.decrypt(
       user.password,
       process.env.PASS_SEC_KEY
